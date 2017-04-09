75395d52ee837623db44570340da42c89a06a277
Eric Bridgeford <ebridge2@jhu.edu>
2017-04-09 03:24:54 -0400

    conversion utilities

---
+++ source/ebridge2/python/fngs_convert/npy2csv.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python2.7
+# a script for converting from npz to csv format.
+# written by eric bridgeford
+# usage:
+#    ./npz2csv.py npzpath csvpath
+#
+import numpy as np
+from argparse import ArgumentParser
+
+def npy2csv(npyname, csvname):
+    data = np.load(npyname)
+    np.savetxt(csvname, data)
+
+def main():
+    parser=ArgumentParser(description="")
+    parser.add_argument("in_file", help="the npy file to convert to csv.")
+    parser.add_argument("out_file", help="the csv file to be converted to.")
+    result=parser.parse_args()
+    npy2csv(result.in_file, result.out_file)
+
+if __name__ == "__main__":
+    main()
+
---
+++ source/ebridge2/python/fngs_convert/npy2rds.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# call from a directory where you want
+# to have your npy files converted to rds files.
+# written by eric bridgeford
+#
+# Usage:
+#   ./npy2rds.sh /path/to/your/roi_timeseries
+#
+
+cd $(dirname "$0")
+
+echo "$(pwd)"
+
+while read -r npyline; do
+    ./npy2csv.py $npyline ${npyline//.npy/.csv}
+    ./csv2rds.R ${npyline//.npy/.csv} ${npyline//.npy/.rds}
+done < <(find $1 -wholename "*ts_roi*.npy")
+
+cd -
+echo "$(pwd)"
