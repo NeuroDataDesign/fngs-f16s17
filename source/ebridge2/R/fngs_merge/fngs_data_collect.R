2be4ff07d2aaf2259fbf41370df3f11fcd36243b
eric bridgeford <ebridge2@jhu.edu>
2017-09-10 14:55:04 -0400

    updated for latest version of fmriutils

--- source/ebridge2/R/fngs_merge/fngs_data_collect.R
+++ source/ebridge2/R/fngs_merge/fngs_data_collect.R
@@ -1,31 +1,28 @@
 # -------- FNGS Data Collect ----------------------------------#
 datasets <- c('BNU1', 'BNU2', 'DC1', 'HNU1', 'IPCAS1', 'IPCAS2', 'IPCAS5', 'IPCAS6',
-              'IPCAS8','IACAS', 'IBATRT', 'NYU1', 'SWU1', 'SWU2', 'SWU3', 'SWU4', 'UM', 'Utah1',
+              'IPCAS8','IACAS', 'IBATRT', 'NYU1', 'SWU1', 'SWU2', 'SWU3', 'SWU4',
               'UWM', 'XHCUMS')
 
-basepath <- 'C:/Users/ebrid/Documents/R/FNGS_results/fngs_corr_0708/rds'
+basepath <- '/data/timeseries'
 
 fngs_scanids <- list()
 fngs_results <- data.frame(dataset=c(), pipeline=c(), discriminability=c())
 performed_dataset <- c()
 for (dataset in datasets) {
-  dset_path <- paste(basepath, dataset, 'timeseries/roi/aal-2mm/', sep='/')
-  fngs_tsnames <- list.files(dset_path, pattern="\\.rds", full.names=TRUE)
-  scan_pos = 2
-  if (length(fngs_tsnames) != 0) {
+  print(dataset)
+  dset_path <- paste(basepath, dataset, 'timeseries/roi/desikan-2mm/', sep='/')
+  fngs_obj <- fmriu.io.open_timeseries(dset_path)
+  signal <- fngs_obj$ts
+  if (length(signal) != 0) {
     performed_dataset <- c(performed_dataset, dataset)
-    print(dataset)
-    fngs_obj <- open_timeseries(fngs_tsnames, sub_pos=scan_pos)
-    signal <- fngs_obj[[1]]
-    fngsids <- fngs_obj[[3]]
-    fngs_scanids[[dataset]] <- cpac_ids
+    fngsids <- fngs_obj$subjects
     d <- tryCatch({
-      time_discr(signal, fngsids, rank=TRUE, graphs = FALSE)$d
+      discr.time.obs2discr(signal, fngsids, rank=TRUE)$d
     },
     error=function(e) {return(NaN)}
     )
     print(d)
     fngs_results <- rbind(fngs_results, data.frame(dataset=dataset, pipeline='FNGS',
-                                                   n=length(fngs_tsnames), discriminability=d))
+                                                   n=length(fngsids), discriminability=d))
   }
 }
